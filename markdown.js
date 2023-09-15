73e181bff2dad33eef0a6cdac30837cbc64e22c6
IanHawkins <111469957+NovaHawkins@users.noreply.github.com>
2023-09-15 13:53:19 -0700

    Update markdown.js

--- markdown.js
+++ markdown.js
@@ -14,6 +14,6 @@ This is **bold** and *italic* text.
 
 const htmlContent = MarkdownTransformer.convertMarkdownToHTML(markdownContent)
 
-const body = document.getElementById("markdown-content")
+const body = document.getElementById('markdown-content')
 
 body.innerHTML = body
